f134a775af5c464245c799ebe4e98b24d7904d7a
Eduard  Farinyes Gasalla <Eduard@gmail.com>
2022-08-07 19:11:50 +0200

    feat: Modificat package.json i ampliat compatibilitat components

--- src/components/Mylabel.tsx
+++ src/components/Mylabel.tsx
@@ -44,3 +44,5 @@ export const Mylabel = ( {
     </span>
   )
 }
+
+export default Mylabel;
--- src/stories/Page.tsx
+++ src/stories/Page.tsx
@@ -78,3 +78,5 @@ export const Page: React.VFC = () => {
     </article>
   );
 };
+
+export default Page;
